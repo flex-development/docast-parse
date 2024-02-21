1eff5a14ea0bedd1b8db10067d6a3200b806965f
Lexus Drumgold <unicornware@flexdevelopment.llc>
2024-02-20 21:19:42 -0500

    chore(github): add commit scope `chore`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -18,7 +18,7 @@ import { scopes } from '@flex-development/commitlint-config'
 const config: UserConfig = {
   extends: ['@flex-development'],
   rules: {
-    'scope-enum': [Severity.Error, 'always', scopes(['plugin'])]
+    'scope-enum': [Severity.Error, 'always', scopes(['chore', 'plugin'])]
   }
 }
 
